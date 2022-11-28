c7821a51cc2e01af6c4bcf1517cefd7d1b0e496c
liqiujiong <pelle@live.cn>
2022-11-28 13:51:59 +0800

    feat: switch

--- app/src/main/java/com/example/ttsapplication/MainActivity.java
+++ app/src/main/java/com/example/ttsapplication/MainActivity.java
@@ -10,8 +10,18 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private Button speakNowButton;
+    private Button speakAddButton;
+    private Button speakStopButton;
+    private Button speakIsPlayButton;
+    private Button speakSwitchButton;
     private EditText editText;
     private TTSManager ttsManager = null;
+
+    @Override
+    protected void onDestroy() {
+        ttsManager.shutDown();
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,11 @@ public class MainActivity extends AppCompatActivity {
 
         editText = (EditText) findViewById(R.id.input_text);
         speakNowButton = (Button) findViewById(R.id.speak_now);
+        speakAddButton = (Button) findViewById(R.id.speak_add);
+        speakStopButton = (Button) findViewById(R.id.speak_stop);
+        speakIsPlayButton = (Button) findViewById(R.id.speak_isplay);
+        speakSwitchButton = (Button) findViewById(R.id.speak_switch);
+
         speakNowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View arg0) {
@@ -32,8 +47,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        speakAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                String text = editText.getText().toString();
+                ttsManager.addQueue(text);
+            }
+        });
+
+
+        speakStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ttsManager.onStop();
+            }
+        });
+
+        speakIsPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                boolean res = ttsManager.isSpeaking();
+                editText.setText(Boolean.toString(res));
+            }
+        });
+
+        speakSwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                ttsManager.switchLanguage();
+            }
+        });
+
 
 
     }
+
+
 
 }
--- app/src/main/java/com/example/ttsapplication/TTSManager.java
+++ app/src/main/java/com/example/ttsapplication/TTSManager.java
@@ -17,10 +17,10 @@ public class TTSManager {
                 Log.e("TTS status",Integer.toString(i));
                 if (i == TextToSpeech.SUCCESS) {
                     int result = mTts.setLanguage(Locale.CHINA);
-                    mTts.setPitch(0.5f);// Set the tone, the higher the value, the sharper the voice (female), the smaller the value, it becomes a male voice. 1.0 is routine.
+                    mTts.setPitch(1f);// Set the tone, the higher the value, the sharper the voice (female), the smaller the value, it becomes a male voice. 1.0 is routine.
                     mTts.setSpeechRate(1.0f);
                     isLoaded = true;
-//                    mTts.setOnUtteranceProgressListener(TTSUtils.this);
+//                    mTts.setOnUtterance12345ProgressListener(TTSUtils.this);
 //                    if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
 //                        //系统不支持中文播报
 //                        isSuccess = false;
@@ -36,11 +36,15 @@ public class TTSManager {
 
 
     public void shutDown() {
-        mTts.shutdown();
+        if (mTts != null){
+            mTts.stop();
+            mTts.shutdown();
+        }
     }
 
     public void addQueue(String text) {
         if (isLoaded)
+            // TextToSpeech.QUEUE_ADD：该模式下会把新的语音任务放到语音任务之后，等前面的语音任务执行完了才会执行新的语音任务。
             mTts.speak(text, TextToSpeech.QUEUE_ADD, null);
         else
             Log.e("TTS error", "TTS Not Initialized");
@@ -49,8 +53,31 @@ public class TTSManager {
     public void initQueue(String text) {
 
         if (isLoaded)
+            // TextToSpeech.QUEUE_FLUSH：该模式下在有新任务时候会清除当前语音任务，执行新的语音任务
             mTts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
         else
             Log.e("TTS error", "TTS Not Initialized");
     }
+
+    public void onStop(){
+        mTts.stop();
+    }
+
+    public String getLanguage() {
+        Locale res = mTts.getLanguage();
+        return res.toString();
+    }
+
+    public void switchLanguage() {
+
+        mTts.setLanguage(Locale.US);
+//        for(TextToSpeech.EngineInfo engine: mTts.getEngines()) {
+//            Log.e("TTS engine:", engine.name+','+engine.label+',');
+//        }
+    }
+
+    public boolean isSpeaking(){
+        return mTts.isSpeaking();
+    }
+
 }
